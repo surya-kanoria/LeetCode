0e12b13b5973f910e72fb9c317222e2b6fb58b63
Surya Kanoria <suryakanoria@gmail.com>
2019-09-06 17:45:58 -0600

    merge-intervals implemented

---
+++ merge-intervals/merge-intervals.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+        if (intervals.length <= 1) {
+            return intervals;
+        }
+
+        List<int[]> merged = new ArrayList<>();
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            if (intervals[i][0] <= end) {
+                end = Math.max(intervals[i][1], end);
+            } else {
+                int[] list = new int[] { start, end };
+                merged.add(list);
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+
+        int[] list = new int[] { start, end };
+        merged.add(list);
+
+        return merged.toArray(new int[merged.size()][2]);
+    }
+}
