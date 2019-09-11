ffe759c817a126c6150f56ba8a7fab91dd06fab8
Surya Kanoria <suryakanoria@gmail.com>
2019-09-10 22:41:34 -0600

    dummy commit

--- 31.next-permutation/next-permutation.java
+++ 31.next-permutation/next-permutation.java
@@ -1,18 +1,42 @@
 class Solution {
-    public void nextPermutation(int[] nums) {
-        for (int i = nums.length - 1; i > 0; i--) {
-            if (nums[i] > nums[(i - 1)]) {
-                int temp = nums[i];
-                nums[i] = nums[i - 1];
-                nums[i - 1] = temp;
-                return;
+    public void nextPermutation(int[] num) {
+        int n = num.length;
+        if (n < 2)
+            return;
+        int index = n - 1;
+        while (index > 0) {
+            if (num[index - 1] < num[index])
+                break;
+            index--;
+        }
+        if (index == 0) {
+            reverseSort(num, 0, n - 1);
+            return;
+        } else {
+            int val = num[index - 1];
+            int j = n - 1;
+            while (j >= index) {
+                if (num[j] > val)
+                    break;
+                j--;
             }
+            swap(num, j, index - 1);
+            reverseSort(num, index, n - 1);
+            return;
         }
-        for (int i = 0; i < (nums.length / 2); i++) {
-            int pos = (nums.length - 1 - i);
-            int temp = nums[i];
-            nums[i] = nums[pos];
-            nums[pos] = temp;
-        }
+    }
+
+    public void swap(int[] num, int i, int j) {
+        int temp = 0;
+        temp = num[i];
+        num[i] = num[j];
+        num[j] = temp;
+    }
+
+    public void reverseSort(int[] num, int start, int end) {
+        if (start > end)
+            return;
+        for (int i = start; i <= (end + start) / 2; i++)
+            swap(num, i, start + end - i);
     }
 }
---
+++ 416.parition-equal-sum/partition-equal-sum.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int totalSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+        }
+    }
+}
