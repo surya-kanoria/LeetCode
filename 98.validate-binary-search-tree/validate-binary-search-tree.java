d0cdba7a83e6ee196e7146e320095f5a0a44d4ad
Surya Kanoria <suryakanoria@gmail.com>
2019-09-15 14:35:16 -0600

    binary-search-tree-validate

---
+++ 98.validate-binary-search-tree/validate-binary-search-tree.java
@@ -0,0 +1,20 @@
+
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+
+    public boolean isValidBST(TreeNode root) {
+        return getIsValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean getIsValidBST(TreeNode node, int lowerBound, int upperBound) {
+        if (node == null) {
+            return true;
+        } else {
+            return (lowerBound < node.val && node.val < upperBound) && getIsValidBST(node.left, lowerBound, node.val)
+                    && getIsValidBST(node.right, node.val, upperBound);
+        }
+    }
+}
