c6638f6954f6a26a4fed1ca765ea04b93397fabd
Surya Kanoria <suryakanoria@gmail.com>
2019-10-06 00:38:20 -0600

    find median frm data stream implemented

--- 295.find-median-from-data-stream/find-median-from-data-stream.java
+++ 295.find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,4 +1,8 @@
+import java.util.PriorityQueue;
+
 class MedianFinder {
+    PriorityQueue<Integer> min = new PriorityQueue<>();
+    PriorityQueue<Integer> max = new PriorityQueue<>(11, Comparator.reverseOrder());
 
     /** initialize your data structure here. */
     public MedianFinder() {
@@ -6,11 +10,23 @@ class MedianFinder {
     }
 
     public void addNum(int num) {
-
+        if (min.size() == max.size()) {
+            max.offer(num);
+            int n = max.poll();
+            min.offer(n);
+        } else {
+            min.offer(num);
+            int n = min.poll();
+            max.offer(n);
+        }
     }
 
     public double findMedian() {
-
+        if (min.size() == max.size()) {
+            return ((double) min.peek() + (double) max.peek()) / 2;
+        } else {
+            return min.peek();
+        }
     }
 }
 
